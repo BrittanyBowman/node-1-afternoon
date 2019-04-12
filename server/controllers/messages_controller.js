97ecede471e83558b8198ba4316404d54f6cc22f
Brittany Bowman <britpetty@gmail.com>
2019-04-12 10:52:47 -0600

    Finished with notes.

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,34 +1,38 @@
+// create a controller that will handle the logic to create, read, update, and delete messages. 
 
+//create an array that will keep track of all the messages. 
 let messages = [];
-
+// create an id variable that is equal to 0.
 let id = 0;
 
+//use module.exports to export an object. Use methods on this object. Start by creating methods for create, read, update, and delete.
 module.exports = {
-    create : ( req, res ) => {
-        const { text, time } = req.body;
-        messages.push({ id, text, time }); id++;
-        res.status(200).send( messages );
+    create: (req, res) => {
+        const { text, time } = req.body; //object using "text" and "time" from the request body. 
+        messages.push({ id, text, time }); //pushes the new message into the "messages" array.
+        id++; // id increments by 1 for each message pushed onto the array.
+        res.status(200).send(messages); // returns the updated messages array with a successful status.
     },
-    read : ( req, res ) => {
-        res.status(200).send( messages );
+    read: (req, res) => {
+        res.status(200).send(messages); // returns the entire messages array with a successful status.
     },
-    update : ( req, res ) => {
-        const {text} = req.body;
-        const updateID = req.params.id;
-        const messageIndex = messages.findIndex( message => message.id == updateID );
+    update: (req, res) => {
+        const { text } = req.body; //updates the body text of the message.
+        const updateID = req.params.id; //determines which message to update based on the value of "id."
+        const messageIndex = messages.findIndex(message => message.id == updateID); //finds the index in the array where the id's match and, if they are equal(ish), it updates the object and retains the id.
         let message = messages[messageIndex];
 
         messages[messageIndex] = {
             id: message.id,
             text: text || message.text,
-            time: message.time
+            time: message.time 
         };
-        res.status(200).send( messages );
+        res.status(200).send(messages); // then returns the updated messages array with the new information.
     },
-    delete : ( req, res ) => {
-        const deleteID = req.params.id;
-        messageIndex = messages.findIndex( message => message.id == deleteID );
-        messages.splice(messageIndex, 1);
-        res.status(200).send( messages );
+    delete: (req, res) => {
+        const deleteID = req.params.id; // determines which message to delete based on the value of "id."
+        messageIndex = messages.findIndex(message => message.id == deleteID); //see below
+        messages.splice(messageIndex, 1);// removes the message at the index from findIndex().
+        res.status(200).send(messages); //same as "update", but deletes instead.
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require('./controllers/messages_controller');
+const mc = require('./controllers/messages_controller'); //Require the messages controller.
+const baseUrl = "/api/messages"; //set the base url.
 
 const app = express();
 
-const mBaseUrl = "/api/messages";
+//Configure the app to parse JSON from the body.
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
-app.use( bodyParser.json() );
-app.use( express.static( __dirname + '/../public/build' ) );
-app.post( mBaseUrl, mc.create );
-app.get(mBaseUrl, mc.read);
-app.put(`${mBaseUrl}/:id`, mc.update);
-app.delete(`${mBaseUrl}:/id`, mc.delete);
+//Create a post, get, put, and delete endpoint that use the corressponding method on the messages controller. These requests are sent to http://localhost:3001/api/messages.
+app.post(baseUrl, mc.create); // creates a new message. pushes into the array. responds with messages array.
+app.get(baseUrl, mc.read); // reads the messages array. responds with messages array. 
+app.put(`${baseUrl}/:id`, mc.update); // id needed for update. updates by id, then responds with altered messages array.
+app.delete(`${baseUrl}/:id`, mc.delete); // id needed for delete. deletes by id, then responds with altered messages array.
+//For the put and delete endpoints, we need to add on a url parameter of id. A url paramter can be defined by adding :variableName when making the URL for an endpoint.
 
-
+//setup server port
 const port = 3001;
-app.listen( port, () => {
-    console.log(`Server listening on port ${port}.`);
-});
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
+})
